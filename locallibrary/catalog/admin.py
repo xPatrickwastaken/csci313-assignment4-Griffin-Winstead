ba5194eae9fb27d58f3bfe919bf69ecbf096e772
Griffin Winstead <grifwin03@gmail.com>
2024-04-01 16:03:07 -0500

    Did 'Part 4' of the django tutorial. Detailed the 'admin' page of the website, as well as completed the 'challenge yourself' section.

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -2,8 +2,38 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Author)
+#admin.site.register(Book)
+#admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
 admin.site.register(Language)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    
+class BooksInline(admin.TabularInline):
+    model = Book
+  
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+admin.site.register(Author, AuthorAdmin)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre', 'language')
+    inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
