f3791ab97a1c0a88416723736fc92abadff3725d
Griffin Winstead <grifwin03@gmail.com>
2024-04-02 16:50:46 -0500

    Finished 'part 5' of the django tutorial along with the "challenge yourself" portion.

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Generate counts of books/book instances with the word "the"
+    num_books_special_word = Book.objects.filter(title__contains='the').count()
+    num_instances_special_word = BookInstance.objects.filter(imprint__contains='the').count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_books_special_word': num_books_special_word,
+        'num_instances_special_word': num_instances_special_word,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
