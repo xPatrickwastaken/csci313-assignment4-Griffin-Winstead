da22872f4d53900736e02bd1ab84c4f9c422a9cc
Griffin Winstead <grifwin03@gmail.com>
2024-04-03 13:02:37 -0500

    Finished 'part 7' added very basic use of sessions

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -20,6 +20,9 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
 
     context = {
         'num_books': num_books,
@@ -28,6 +31,7 @@ def index(request):
         'num_authors': num_authors,
         'num_books_special_word': num_books_special_word,
         'num_instances_special_word': num_instances_special_word,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
